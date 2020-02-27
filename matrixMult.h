05d9d7144e4bcd19fb3e8753fa3b333590584388
Saurav <musunurusaurav@gmail.com>
2020-02-28 02:42:45 +0530

    Added short forms of Isend and Irecv

--- matrixMult.h
+++ matrixMult.h
@@ -6,16 +6,20 @@
 #include <ctime>
 
 #define WORLD MPI_COMM_WORLD
+#define Init MPI_Init
 #define Finalize MPI_Finalize
 #define Send MPI_Send
 #define Recv MPI_Recv
-#define Init MPI_Init
 #define INT MPI_INT
 #define DOUBLE MPI_DOUBLE
 #define Status MPI_Status
 #define Broadcast MPI_Bcast
 #define Gatherv MPI_Gatherv
 #define Scatterv MPI_Scatterv
+#define Request MPI_Request
+#define Isend MPI_Isend
+#define Irecv MPI_Irecv
+#define Wait MPI_Wait
 
 bool isEqual(double **A, double **B, int m, int n);
 bool isEqual(double *A, double *B, int m, int n);
